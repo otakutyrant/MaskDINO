6da817fa9f11df818cef4353940929028d1c8810
otakutyrant <otakutyrant@gmail.com>
2023-02-14 20:32:05 +0800

    poc_dataset.py: fix typos

--- poc_dataset.py
+++ poc_dataset.py
@@ -5,13 +5,13 @@ from PIL import Image
 def generate_dataset(split):
     file_names = []
     json_path = Path(__file__).parent.absolute()
-    if split = 'train':
+    if split == 'train':
         json_path /= 'train_seg.json'
         for path in Path('dataset-1').iterdir():
             file_names.append(str(path))
         for path in Path('dataset-2').iterdir():
             file_names.append(str(path))
-    elif split == 'val:'
+    elif split == 'val':
         json_path /= 'val_seg.json'
         for path in Path('dataset-3').iterdir():
             file_names.append(str(path))
@@ -42,10 +42,10 @@ def train_dataset_function():
     json_path = Path(__file__).parent.absolute() / 'train_seg.json'
     if not json_path.exists():
         generate_dataset('train')
-    return json.load(open(json_path)
+    return json.load(open(json_path))
 
 def val_dataset_function():
     json_path = Path(__file__).parent.absolute() / 'val_seg.json'
     if not json_path.exists():
         generate_dataset('val')
-    return json.load(open(json_path)
+    return json.load(open(json_path))
